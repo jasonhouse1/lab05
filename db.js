694e2fa137c354d548a490a989fbce5d41901b80
jasonhouse1 <411631327@o365.tku.edu.tw>
2025-05-19 20:58:36 +0800

    Initial commit

--- db.js
+++ db.js
@@ -3,9 +3,10 @@ const mariadb = require('mariadb');
 const pool = mariadb.createPool({
     host: 'localhost',
     user: 'root',
-    password: '930114',
+    password: 'jasonhouse1',
     database: 'university_db',
-    connectionLimit: 5
+    connectionLimit: 10,
+    port: 3307,
 });
 
 module.exports = pool;
